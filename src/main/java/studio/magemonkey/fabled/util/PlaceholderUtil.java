e9a0e5898a87e72de4eb5b8bf80aa99c7d1bfebf
RAYCAL2000 <64821088+RAYCAL2000@users.noreply.github.com>
2025-01-22 17:18:42 -0500

    [Bug] Skill List Placeholders not Alphabetical (#1100)

--- src/main/java/studio/magemonkey/fabled/util/PlaceholderUtil.java
+++ src/main/java/studio/magemonkey/fabled/util/PlaceholderUtil.java
@@ -23,6 +23,7 @@ import studio.magemonkey.fabled.dynamic.DynamicSkill;
 import studio.magemonkey.fabled.hook.PlaceholderAPIHook;
 import studio.magemonkey.fabled.hook.PluginChecker;
 
+import java.lang.reflect.Array;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -863,24 +864,20 @@ public class PlaceholderUtil {
         }
     }
 
-    private static String getSkills(OfflinePlayer player, List<String> arguments, Integer accountId) {
-        try {
-            PlayerData   playerData = getPlayerData(player, accountId);
-            List<String> skills     = new ArrayList<>();
-            for (PlayerSkill skill : playerData.getSkills()) {
-                skills.add(skill.getData().getName());
-            }
-            Collections.sort(skills);
-            return skills.toString();
-        } catch (Exception e) {
-            return "[]";
+    private static List<String> getSkills(OfflinePlayer player, List<String> arguments, Integer accountId) {
+        PlayerData   playerData = getPlayerData(player, accountId);
+        List<String> skills     = new ArrayList<>();
+        for (PlayerSkill skill : playerData.getSkills()) {
+            skills.add(skill.getData().getName());
         }
+        Collections.sort(skills);
+        return skills;
     }
 
     // Returns a list of all the skills a player current has. [] if not found.
     private static String skillsPlaceholder(OfflinePlayer player, List<String> arguments, Integer accountId) {
         try {
-            return getSkills(player, arguments, accountId);
+            return getSkills(player, arguments, accountId).toString();
         } catch (Exception e) {
             return "[]";
         }
@@ -889,7 +886,7 @@ public class PlaceholderUtil {
     // Returns a list of all the skills a player current has. "" if not found.
     private static String formatSkillsPlaceholder(OfflinePlayer player, List<String> arguments, Integer accountId) {
         try {
-            return getSkills(player, arguments, accountId).replaceAll("(^\\[|\\]$)", "");
+            return getSkills(player, arguments, accountId).toString().replaceAll("(^\\[|\\]$)", "");
         } catch (Exception e) {
             return "";
         }
@@ -898,9 +895,7 @@ public class PlaceholderUtil {
     // Returns the name of a skill at the specified location. "" if not found.
     private static String skillsNamePlaceholder(OfflinePlayer player, List<String> arguments, Integer accountId) {
         try {
-            PlayerData playerData = getPlayerData(player, accountId);
-            return playerData.getSkills().toArray(new PlayerSkill[0])[Integer.parseInt(arguments.remove(0))
-                    - 1].getData().getName();
+            return getSkills(player, arguments, accountId).get(Integer.parseInt(arguments.remove(0))-1);
         } catch (Exception e) {
             return "";
         }
@@ -909,10 +904,7 @@ public class PlaceholderUtil {
     // Returns the information of a skill at the specified location and given placeholder. "" if not found.
     private static String skillsInfoPlaceholder(OfflinePlayer player, List<String> arguments, Integer accountId) {
         try {
-            PlayerData playerData = getPlayerData(player, accountId);
-            String skillName = playerData.getSkills().toArray(new PlayerSkill[0])[Integer.parseInt(arguments.remove(0))
-                    - 1].getData().getName();
-            arguments.add(skillName);
+            arguments.add(getSkills(player, arguments, accountId).get(Integer.parseInt(arguments.remove(0))-1));
             return actions.getOrDefault(arguments.remove(0), (a, b, c) -> null).apply(player, arguments, accountId);
         } catch (Exception e) {
             return "";
