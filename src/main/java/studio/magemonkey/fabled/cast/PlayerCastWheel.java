ba3069c6188b59324d6effe289d98a6a81d36d24
RAYCAL2000 <rcal2500@gmail.com>
2025-07-11 14:49:44 -0400

    Working on new castmode

---
+++ src/main/java/studio/magemonkey/fabled/cast/PlayerCastWheel.java
@@ -0,0 +1,105 @@
+package studio.magemonkey.fabled.cast;
+
+import lombok.Getter;
+import studio.magemonkey.codex.util.StringUT;
+import studio.magemonkey.fabled.Fabled;
+import studio.magemonkey.fabled.api.player.PlayerData;
+import studio.magemonkey.fabled.api.player.PlayerSkill;
+
+import java.util.ArrayList;
+
+public class PlayerCastWheel {
+    private final PlayerData player;
+    private final ArrayList<PlayerSkill> skills = new ArrayList<PlayerSkill>();
+    @Getter
+    private boolean casting = false;
+    private int index;
+
+    public PlayerCastWheel(PlayerData data) {
+        this.player = data;
+        this.index = 0;
+        this.skills.clear();
+
+        for (PlayerSkill skill : data.getSkills()){
+            if (skill.getData().canCast() && skill.isUnlocked()) skills.add(skill);
+        }
+    }
+
+    public void validate(PlayerData data){
+        for (PlayerSkill skill : data.getSkills()){
+            if (skill.getData().canCast() && skill.isUnlocked()) skills.add(skill);
+        }
+    }
+
+    /**
+     * Adds a skill to the available skills, if castable
+     *
+     * @param skill skill to add
+     */
+    public void unlock(PlayerSkill skill) {
+        if (skill.isUnlocked() && skill.getData().canCast())
+            skills.add(skill);
+    }
+
+    public boolean isEmpty(){
+        return skills.isEmpty();
+    }
+
+public String getMessage() {
+    int size = skills.size();
+    if (size == 0) return "";
+
+    int currentIndex = index % size;
+    if (currentIndex < 0) currentIndex += size; // Handle negatives
+
+    String skillFormat = StringUT.color(Fabled.getSettings().getMessageFormatSkill());
+    String separator   = StringUT.color(Fabled.getSettings().getMessageFormatSeparator());
+
+    int prevIndex = (currentIndex - 1 + size) % size;
+    int nextIndex = (currentIndex + 1) % size;
+
+    StringBuilder stringBuilder = new StringBuilder();
+
+    // Append previous
+    stringBuilder.append(skillFormat
+        .replace("%number%", String.valueOf(prevIndex + 1))
+        .replace("%skill%", skills.get(prevIndex).getData().getName()));
+
+    // Append separator + current
+    stringBuilder.append(separator)
+        .append(skillFormat
+        .replace("%number%", String.valueOf(currentIndex + 1))
+        .replace("%skill%", skills.get(currentIndex).getData().getName()));
+
+    // Append separator + next
+    stringBuilder.append(separator)
+        .append(skillFormat
+        .replace("%number%", String.valueOf(nextIndex + 1))
+        .replace("%skill%", skills.get(nextIndex).getData().getName()));
+    return stringBuilder.toString();
+}
+
+
+    public void setCasting(boolean casting) {
+        this.casting = casting;
+    }
+
+    public void cast() {
+        if (!skills.isEmpty()){
+            player.cast(skills.get(index));
+        }
+    }
+
+    public void nextSkill() {
+        if (!skills.isEmpty()) {
+            index = (index + 1) % skills.size();
+        }
+    }
+
+    public void previousSkill() {
+        if (!skills.isEmpty()) {
+            index = (index + skills.size() - 1) % skills.size();
+        }
+    }
+
+}
---
+++ src/main/java/studio/magemonkey/fabled/listener/CastWheelListener.java
@@ -0,0 +1,151 @@
+package studio.magemonkey.fabled.listener;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import studio.magemonkey.fabled.Fabled;
+import studio.magemonkey.fabled.api.event.PlayerSkillUnlockEvent;
+import studio.magemonkey.fabled.api.player.PlayerData;
+import studio.magemonkey.fabled.cast.CastMode;
+import studio.magemonkey.fabled.cast.PlayerCastWheel;
+import studio.magemonkey.fabled.hook.PlaceholderAPIHook;
+import studio.magemonkey.fabled.hook.PluginChecker;
+
+public class CastWheelListener extends FabledListener {
+    private       boolean                    enabled = true;
+
+    @Override
+    public void init() {
+        MainListener.registerJoin(this::init);
+        Bukkit.getOnlinePlayers().forEach(this::init);
+    }
+
+    @Override
+    public void cleanup() {
+        Bukkit.getOnlinePlayers().stream()
+                .filter(player -> Fabled.getSettings().isWorldEnabled(player.getWorld()))
+                .forEach(this::init);
+        this.enabled = false;
+    }
+
+    private void init(Player player) {
+        PlayerData data = Fabled.getData(player);
+        data.getCastWheel().validate(data);
+    }
+
+    /**
+     * Clears skill bars upon quitting the game
+     *
+     * @param event event details
+     */
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onQuit(PlayerQuitEvent event) {
+        if (Fabled.getSettings().isWorldEnabled(event.getPlayer().getWorld()))
+            init(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onChangeWorldPre(PlayerChangedWorldEvent event) {
+        if (!Fabled.getSettings().isWorldEnabled(event.getPlayer().getWorld()) && Fabled.getSettings()
+                .isWorldEnabled(event.getFrom()))
+            init(event.getPlayer());
+    }
+
+    /**
+     * Adds unlocked skills to the skill bar if applicable
+     *
+     * @param event event details
+     */
+    @EventHandler
+    public void onUnlock(PlayerSkillUnlockEvent event) {
+        event.getPlayerData().getCastWheel().unlock(event.getUnlockedSkill());
+    }
+
+    public class CastWheelTask extends BukkitRunnable {
+        private final PlayerData playerData;
+
+        public CastWheelTask(PlayerData playerData) {
+            this.playerData = playerData;
+        }
+
+        @Override
+        public void run() {
+            Player player = playerData.getPlayer();
+            if (!playerData.getCastWheel().isCasting()) {
+                this.cancel();
+                return;
+            }
+            String message = playerData.getCastWheel().getMessage();
+            if (PluginChecker.isPlaceholderAPIActive()) {
+                message = PlaceholderAPIHook.format(message, player);
+            }
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(message));
+            }
+
+                    @Override
+        public synchronized void cancel() throws IllegalStateException {
+            super.cancel();
+            Player player = playerData.getPlayer();
+            if (player != null) player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(""));
+            }
+        }
+
+    @EventHandler
+    public void onHandSwap(PlayerSwapHandItemsEvent event) {
+        Player player = event.getPlayer();
+        // Allow hand swap if Player is sneaking
+        if (player.isSneaking()) return;
+        event.setCancelled(true);
+        PlayerData playerData = Fabled.getData(player);
+        PlayerCastWheel castWheel = playerData.getCastWheel();
+        if (castWheel.isCasting()) {
+            castWheel.setCasting(false);
+        } else if (!castWheel.isEmpty()) {
+            castWheel.setCasting(true);
+            new CastWheelTask(playerData).runTaskTimer(Fabled.inst(), 0, 1);
+        }
+    }
+
+    @EventHandler
+    public void onHeld(PlayerItemHeldEvent event) {
+        Player player = event.getPlayer();
+        PlayerData playerData = Fabled.getData(player);
+        PlayerCastWheel castWheel = playerData.getCastWheel();
+
+        // Return if Player is not Casting
+        // Return if Player is Sneaking
+        // Otherwise cancel scroll, change wheel
+        if (!castWheel.isCasting()) return;
+        if (player.isSneaking()) return;
+        event.setCancelled(true);
+
+        int previousSlot = event.getPreviousSlot();
+        int newSlot = event.getNewSlot();
+        int direction = (newSlot - previousSlot + 9) % 9;
+        if (direction == 1) {
+            castWheel.nextSkill();
+        } else if (direction == 8) {
+            castWheel.previousSkill();
+        }
+    }
+
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent event) {
+        Player player = event.getPlayer();
+        PlayerData playerData = Fabled.getData(player);
+        PlayerCastWheel castWheel = playerData.getCastWheel();
+        if (!castWheel.isCasting()) return;
+        event.setCancelled(true);
+        castWheel.cast();
+    }
+
+}
